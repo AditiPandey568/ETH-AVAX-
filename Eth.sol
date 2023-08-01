436d8764616c723f0003ec37fa631c1197b0eefe
Aditi Pandey <136813656+AditiPandey568@users.noreply.github.com>
2023-08-01 18:28:35 +0530

    Update Eth.sol

--- Eth.sol
+++ Eth.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract ExampleFunctions {
+    // Function demonstrating the use of require
+    function requireFunction(uint256 _value) external pure returns (bool) {
+        require(_value > 0, "Value must be greater than zero");
+        return true;
+    }
+
+    // Function demonstrating the use of assert
+    function assertFunction(uint256 _value) external pure returns (uint256) {
+        uint256 minValue = 100;
+        assert(_value >= minValue);
+        return _value;
+    }
+
+    // Function demonstrating the use of revert
+    function revertFunction(uint256 _value) external pure returns (uint256) {
+        if (_value > 1000) {
+            revert("Value cannot be greater than 1000");
+        }
+        return _value;
+    }
+}
