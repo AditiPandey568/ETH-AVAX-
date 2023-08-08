6b3de3003ebcad2ae9f1a8fdfae9ac8620249fdd
Aditi Pandey <136813656+AditiPandey568@users.noreply.github.com>
2023-08-08 19:22:07 +0530

    Eth-avax.sol

--- Eth.sol
+++ Eth.sol
@@ -1,42 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract HospitalManagement {
-    struct Patient {
+contract LibraryMembership {
+    struct Member {
         uint id;
         string name;
-        bool admitted;
+        bool isActive;
+        uint booksBorrowed;
     }
     
-    mapping(uint => Patient) public patients;
-    uint public patientCount;
+    struct Book {
+        uint id;
+        string title;
+        bool available;
+    }
     
+    mapping(uint => Member) public members;
+    mapping(uint => Book) public books;
+    uint public memberCount;
+    uint public bookCount;
     
-    function admitPatient(uint _id, string memory _name) public {
-        // require statement
-        require(!patients[_id].admitted, "Patient is already admitted");
-        
-        // assert statement
+    constructor() {
+        addBook("Introduction to Solidity");
+        addBook("Blockchain Basics");
+    }
+    
+    function addMember(uint _id, string memory _name) public {
+        require(!members[_id].isActive, "Member is already active");
         assert(bytes(_name).length > 0);
         
-        patients[_id] = Patient(_id, _name, true);
-        patientCount++;
+        members[_id] = Member(_id, _name, true, 0);
+        memberCount++;
     }
     
-    function dischargePatient(uint _id) public {
-        // require statement
-        require(patients[_id].admitted, "Patient is not admitted");
+    function removeMember(uint _id) public {
+        require(members[_id].isActive, "Member is not active");
         
-        patients[_id].admitted = false;
-        patientCount--;
+        members[_id].isActive = false;
+        memberCount--;
     }
     
-    function updatePatientName(uint _id, string memory _name) public {
-        // revert statement
-        if (bytes(_name).length == 0) {
-            revert("Name cannot be empty");
-        }
+    function borrowBook(uint _memberId, uint _bookId) public {
+        require(members[_memberId].isActive, "Member is not active");
+        require(books[_bookId].available, "Book is not available");
         
-        patients[_id].name = _name;
+        members[_memberId].booksBorrowed++;
+        books[_bookId].available = false;
+    }
+    
+    function returnBook(uint _memberId, uint _bookId) public {
+        require(members[_memberId].isActive, "Member is not active");
+        require(!books[_bookId].available, "Book is already returned");
+        
+        members[_memberId].booksBorrowed--;
+        books[_bookId].available = true;
+    }
+    
+    function addBook(string memory _title) public {
+        assert(bytes(_title).length > 0);
+        
+        books[bookCount] = Book(bookCount, _title, true);
+        bookCount++;
     }
 }
